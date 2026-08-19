fb37d121d2e07469274d43cb6b0c829433e69c03
PrateekPersoya <2024ucp1813@mnit.ac.in>
2026-08-19 12:42:48 +0530

    Update dictionary_scoring.py

--- attacks/shift_cipher_attack/src/dictionary_scoring.py
+++ attacks/shift_cipher_attack/src/dictionary_scoring.py
@@ -1,49 +1,54 @@
 from shift_cipher import decrypt
 
 
-def load_words(english_words.txt):
-
-    file = open(english_words.txt, "r")
-
+def load_words(filename):
+    file = open(filename, "r")
     words = file.read().split()
-
     file.close()
 
     return words
 
 
-def dictionary_score(text, words):
-
-    score = 0
+def score(text, words):
+    count = 0
 
     for word in text.split():
-
         word = word.lower()
 
         if word in words:
-            score += 1
+            count += 1
 
-    return score
+    return count
 
 
 def dictionary_attack(ciphertext, words):
-
     best_key = 0
     best_score = 0
     best_text = ""
 
     for key in range(26):
-
         text = decrypt(ciphertext, key)
 
-        score = dictionary_score(text, words)
+        current_score = score(text, words)
 
-        print("Key", key, "Score", score, ":", text)
+        print(key, text, current_score)
 
-        if score > best_score:
-
-            best_score = score
+        if current_score > best_score:
+            best_score = current_score
             best_key = key
             best_text = text
 
     return best_key, best_text
+
+
+# Load the dictionary
+words = load_words("english.txt")
+
+# Test ciphertext
+ciphertext = "khoor zruog"
+
+# Run dictionary attack
+best_key, best_text = dictionary_attack(ciphertext, words)
+
+print("\nBest key:", best_key)
+print("Decrypted text:", best_text)
